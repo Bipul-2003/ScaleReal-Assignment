91e4793923c0579d6e3f90467f5aa7adc7f1cb57
Bipul Swain <bipulswain365@gmail.com>
2024-02-24 17:46:25 +0530

    quick fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,38 +1,48 @@
-import React, { useState } from 'react'
-import { searchMovie } from '../store/movieSlice'
-import { useDispatch } from 'react-redux'
+import React, { useState } from "react";
+import { searchMovie } from "../store/movieSlice";
+import { useDispatch } from "react-redux";
 
+const SearchBar = ({}) => {
+  const dispatch = useDispatch();
 
-const SearchBar = ({ }) => {
-    const dispatch = useDispatch()
-
-    const handelChange = (e) => {
-        e.preventDefault()
-
-        // Used setTimeout inorder to reduce the requests to the store
-        setTimeout(() => {
-            dispatch(searchMovie(e.target.value.toLowerCase()))
-        }, 800);
-    }
-    // console.log(serchString)
-    return (
-        <div className='min- mx-auto'>
-            <div className="relative flex items-center w-full h-9 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-                <div className="grid place-items-center h-full w-12 text-gray-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                </div>
-
-                <input
-                    className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
-                    type="text"
-                    id="search"
-                    placeholder="Type to search.."
-                    onChange={handelChange} />
-            </div>
+  const handelChange = (e) => {
+    e.preventDefault();
+    // clearTimeout(ref);
+    // Used setTimeout inorder to reduce the requests to the store
+     setTimeout(() => {
+      dispatch(searchMovie(e.target.value.toLowerCase()));
+    }, 800);
+  };
+  // console.log(serchString)
+  return (
+    <div className="min- mx-auto">
+      <div className="relative flex items-center w-full h-9 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+        <div className="grid place-items-center h-full w-12 text-gray-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
         </div>
-    )
-}
 
-export default SearchBar
+        <input
+          className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+          type="text"
+          id="search"
+          placeholder="Type to search.."
+          onChange={handelChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
